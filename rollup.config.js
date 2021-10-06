5d7e06bbd12cd11cd2fa073a9beb2d075419ad0e
ChoSeoHwan <wtae1216@naver.com>
2021-10-06 22:19:49 +0900

    chore: rollup 세팅 완료

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import buildRollupOptions, { Input, OutputBuilder } from '@choseohwan/rollup-builder';
+import buildBasePlugins from '@choseohwan/rollup-plugin-builder-base';
+
+const input = new Input('src/index.ts');
+
+const outputBuilder = new OutputBuilder({
+    sourcemap: true
+});
+
+const plugins = buildBasePlugins();
+
+export default buildRollupOptions(
+    input,
+    [
+        outputBuilder.buildCJS({
+            exports: 'named',
+            dir: 'dist/cjs'
+        })
+    ],
+    plugins
+);
